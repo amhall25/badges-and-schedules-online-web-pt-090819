549700b615c1bbd74738206443d57a12a8ed5b8f
alycia hall <amhall25@gmail.com>
2019-09-15 23:43:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  array.each do |name|
+    return "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  array.each_with_index do |name, index|
+    return "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
