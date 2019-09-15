ddbd033fcbea2a75db94081182f2fcb4c0b3c1fb
alycia hall <amhall25@gmail.com>
2019-09-15 23:45:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.each do |name|
+def batch_badge_creator(array)
+  array.each do |name|
     return "Hello, my name is #{name}."
   end
 end
